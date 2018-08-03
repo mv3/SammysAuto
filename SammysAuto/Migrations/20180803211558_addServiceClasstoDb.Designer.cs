71160a0444e25744796b23247ffd83ce92e50944
MV <mattveit1@gmail.com>
2018-08-03 16:17:05 -0500

    Add Service class

--- SammysAuto/Migrations/20180803211558_addServiceClasstoDb.Designer.cs
+++ SammysAuto/Migrations/20180803211558_addServiceClasstoDb.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SammysAuto.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180803211558_addServiceClasstoDb")]
+    partial class addServiceClasstoDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
